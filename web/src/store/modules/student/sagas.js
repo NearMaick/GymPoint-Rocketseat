3ed209d597401599b47deb7de79adad143036ac7
Maick Souza <maick_a_s@msn.com>
2019-11-24 17:22:09 -0300

    Feat: Student Registration

--- web/src/store/modules/student/sagas.js
+++ web/src/store/modules/student/sagas.js
@@ -1,3 +1,22 @@
-import { all } from 'redux-saga/effects';
+import { all, call, takeLatest } from 'redux-saga/effects';
+import api from '../../../services/api';
+import history from '../../../services/history';
 
-export default all([]);
+export function* registerStudent({ payload }) {
+  const { name, email, age, weight, height } = payload;
+
+  yield call(api.post, 'student', {
+    name,
+    email,
+    age,
+    weight,
+    height,
+    is_active: false,
+  });
+
+  // history.push('/');
+}
+
+export default all([
+  takeLatest('@student/REGISTER_STUDENT_REQUEST', registerStudent),
+]);
