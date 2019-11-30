f463acb91f27dda8d28bb529b7efa69c6ce9aeea
Maick Souza <maick_a_s@msn.com>
2019-11-30 16:24:39 -0300

    Initial settings

---
+++ mobile/src/pages/HelpOrder/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+import Background from '../../components/Background';
+
+// import { Container } from './styles';
+
+export default function HelpOrder() {
+  return (
+    <Background>
+      <Text>Help Order</Text>
+    </Background>
+  );
+}
