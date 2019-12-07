eafb9f55eea444a96ea5e5189b74fecb7cd17fc6
Maick Souza <maick_a_s@msn.com>
2019-12-07 15:04:26 -0300

    Feat: authentication

--- web/src/pages/SignIn/index.js
+++ web/src/pages/SignIn/index.js
@@ -1,7 +1,34 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { signInRequest } from '../../store/modules/auth/actions';
+
+import logo from '../../assets/logo.svg';
+
+import { Container } from './styles';
 
 export default function SignIn() {
-  return <h1>SignIn</h1>;
+  const dispatch = useDispatch();
+
+  function handleSubmit({ email, password }) {
+    dispatch(signInRequest(email, password));
+  }
+
+  return (
+    <Container>
+      <img src={logo} alt="GymPoint" />
+
+      <Form onSubmit={handleSubmit}>
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Sua senha secreta"
+        />
+
+        <button type="submit">Entrar no sistema</button>
+      </Form>
+    </Container>
+  );
 }
--- web/src/store/modules/rootSaga.js
+++ web/src/store/modules/rootSaga.js
@@ -1,7 +1,8 @@
 import { all } from 'redux-saga/effects';
 
 import student from './student/sagas';
+import auth from './auth/sagas';
 
 export default function* rootSaga() {
-  return yield all([student]);
+  return yield all([student, auth]);
 }
