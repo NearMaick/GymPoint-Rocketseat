03278ed6b24966840d0aa9eb435855e062dbd425
Maick Souza <maick_a_s@msn.com>
2020-02-25 14:19:26 -0300

    refactor(web): updating for @unform 2.0 #1

--- web/src/pages/Registration/Create/index.js
+++ web/src/pages/Registration/Create/index.js
@@ -1,49 +1,29 @@
-import React, { useEffect, useState } from 'react';
-import DatePicker from 'react-datepicker';
-import { useDispatch } from 'react-redux';
-import ReactSelect from 'react-select';
+import React, { useState, useEffect } from 'react';
 
-import 'react-datepicker/dist/react-datepicker.css';
-import { Form, Input } from '@rocketseat/unform';
+import { Form } from '@unform/web';
 import { startOfDay, format, addDays } from 'date-fns';
 import pt from 'date-fns/locale/pt';
-import * as Yup from 'yup';
 
+import DatePicker from '~/components/Form/DatePicker';
+import Input from '~/components/Form/Input';
+import Select from '~/components/Form/Select';
 import api from '~/services/api';
-import { registerRegistrationRequest } from '~/store/modules/registration/actions';
 
 import { Container } from './styles';
-
-const schema = Yup.object().shape({
-  optsStudent: Yup.string(),
-  optsPlan: Yup.string(),
-  initialDate: Yup.date(),
-  stdId: Yup.number(),
-  plnId: Yup.number(),
-  initial_date: Yup.date(),
-  end_date: Yup.string(),
-  total: Yup.number(),
-});
 
 export default function Student() {
   const [student, setStudent] = useState([]);
   const [plan, setPlan] = useState([]);
 
-  const [month, setMonth] = useState('');
-  const [startDate, setStartDate] = useState(startOfDay(new Date()));
-
-  const [endDate, setEndDate] = useState(
-    format(startOfDay(new Date()), 'dd/MM/yyyy')
-  );
   const [showEndDate, setShowEndDate] = useState(
     format(startOfDay(new Date()), 'dd/MM/yyyy')
   );
 
   const [price, setPrice] = useState('');
 
-  const [studentId, setStudentId] = useState();
-  const [planId, setPlanId] = useState();
-  const [initialDate, setInitialDate] = useState(new Date());
+  const [startDate, setStartDate] = useState(startOfDay(new Date()));
+
+  const [month, setMonth] = useState('');
 
   useEffect(() => {
     async function loadPlans() {
@@ -67,7 +47,7 @@ export default function Student() {
     }
     loadPlans();
     loadStudents();
-  }, [initialDate, startDate]);
+  }, []);
   /**
    * Loading lists on inputs selects
    */
@@ -82,46 +62,32 @@ export default function Student() {
     ...plans,
   }));
 
-  function getStudent(students) {
-    setStudentId(students.value);
-  }
-
   function calcEndDate(date) {
-    setInitialDate(format(date, 'yyyy-MM-dd'));
     setStartDate(date);
-    setEndDate(format(addDays(date, month * 30), 'yyyy-MM-dd'));
     setShowEndDate(format(addDays(date, month * 30), 'dd/MM/yyyy'));
   }
 
   function calcPrice(value) {
     setPrice(value.price * value.duration);
     setMonth(value.duration);
-    setPlanId(value.id);
+    // setPlanId(value.id);
   }
 
-  const dispatch = useDispatch();
-
-  function handleSubmit({ stdId, plnId, initial_date, end_date, total }) {
-    end_date = endDate;
-    // TODO
-    dispatch(
-      registerRegistrationRequest(stdId, plnId, initial_date, end_date, total)
-    );
+  async function handleSubmit(data) {
+    console.tron.log(data);
   }
 
   return (
     <Container>
-      <h1>Cadastro de Matrícula</h1>
-      <Form schema={schema} onSubmit={handleSubmit}>
-        <ReactSelect
+      <Form onSubmit={handleSubmit}>
+        <Select
           name="optsStudent"
           isSearchable
           options={optStudent}
           placeholder="Selecione..."
-          onChange={value => getStudent(value)}
         />
         <div>
-          <ReactSelect
+          <Select
             name="optsPlan"
             options={optPlan}
             isSearchable={false}
@@ -129,20 +95,17 @@ export default function Student() {
             onChange={value => calcPrice(value)}
           />
           <DatePicker
-            name="initialDate"
+            name="date"
             dateFormat="dd/MM/yyyy"
             locale={pt}
             onChange={date => calcEndDate(date)}
             selected={startDate}
             value={this}
           />
-          <Input type="hidden" name="stdId" value={studentId} />
-          <Input type="hidden" name="plnId" value={planId} />
-          <Input type="hidden" name="initial_date" value={initialDate} />
           <Input name="end_date" value={showEndDate} />
           <Input name="total" value={price} />
         </div>
-        <button type="submit">Enviar dados</button>
+        <button type="submit">Enviar</button>
       </Form>
     </Container>
   );
